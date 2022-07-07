a3a00ac0a5ae3a374ec41c3d1c535cfbef3fe44b
sailathatammana <32009979+sailathatammana@users.noreply.github.com>
2022-07-07 16:38:44 +0200

    Added Welcome

---
+++ src/com/company/Display.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Display {
+
+    public static void welcomeMsg() {
+        System.out.println(" Welcome to Spotifoo");
+        System.out.println("------------------------");
+    }
+}

798cf71b42734842d6c1c07f373af03ef431c420
sailathatammana <32009979+sailathatammana@users.noreply.github.com>
2022-07-07 16:50:59 +0200

    Added Main menu

--- src/com/company/Display.java
+++ src/com/company/Display.java
@@ -6,4 +6,15 @@ public class Display {
         System.out.println(" Welcome to Spotifoo");
         System.out.println("------------------------");
     }
+
+    public static void mainMenu(){
+        System.out.println(" Main menu options:");
+        System.out.println(" 1. Songs");
+        System.out.println(" 2. Artists");
+        System.out.println(" 3. Albums");
+        System.out.println(" 4. Genres");
+        System.out.println(" 5. Search\n");
+        System.out.print(" Choose an option and press enter: ");
+
+    }
 }

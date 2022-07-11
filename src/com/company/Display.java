fab705a147e2cd683b485342f21f5543cb76f5c5
sailathatammana <32009979+sailathatammana@users.noreply.github.com>
2022-07-11 14:01:21 +0200

    Case statement

--- src/com/company/Display.java
+++ src/com/company/Display.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Display {
 
     public static void welcomeMsg() {
@@ -13,8 +15,26 @@ public class Display {
         System.out.println(" 2. Artists");
         System.out.println(" 3. Albums");
         System.out.println(" 4. Genres");
-        System.out.println(" 5. Search\n");
+        System.out.println(" 5. Search");
+        System.out.println(" 6. Quit\n");
         System.out.print(" Choose an option and press enter: ");
-
     }
+
+    public static int readAndValidateChoice(){
+        Scanner sc = new Scanner(System.in);
+        int choice = 50;
+        do {
+
+            if(choice <= 0)
+                System.out.println("Please enter a number between 1 to 4!");
+
+            while (!sc.hasNextInt()) {
+                System.out.println("That's not a number!");
+                sc.next();
+            }
+            choice = sc.nextInt();
+        } while (choice <= 0);
+        return choice;
+    }
+
 }

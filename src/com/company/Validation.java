6141dfd12357a5699a637c42c084c0a88a9eccf5
sailathatammana <32009979+sailathatammana@users.noreply.github.com>
2022-07-14 10:04:59 +0200

    Validations update

--- src/com/company/Validation.java
+++ src/com/company/Validation.java
@@ -1,27 +1,31 @@
 package com.company;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Validation {
 
     public static int readAndValidateChoice() {
         Scanner sc = new Scanner(System.in);
+        String inputValue;
         int choice = 50;
-        do {
-
-            if (choice <= 0) {
-                System.out.println("Please enter a number between 1 to 4!");
+        int check = 0;
+        while (check != 1) {
+            try {
+                inputValue = sc.nextLine();
+                choice = Integer.parseInt(inputValue);
+                while (choice > 6 || choice <= 0) {
+                    System.out.println(" Please enter a number between 1 to 6!");
+                    System.out.print(" Choose an option and press enter: ");
+                    inputValue = sc.nextLine();
+                    choice = Integer.parseInt(inputValue);
+                }
+                check = 1;
+            } catch (NumberFormatException e) {
+                System.out.println(" That's not a number!");
                 System.out.print(" Choose an option and press enter: ");
             }
-
-            while (!sc.hasNextInt()) {
-                System.out.println("That's not a number!");
-                System.out.print(" Choose an option and press enter: ");
-                sc.next();
-            }
-            choice = sc.nextInt();
-        } while (choice <= 0);
+        }
         return choice;
     }
-
 }

f9a8cda5d6a1356576d4aabac5201240199fdf7b
sailathatammana <32009979+sailathatammana@users.noreply.github.com>
2022-07-20 16:24:22 +0200

    Refactored and added extra classes

--- src/com/company/ChooseOption.java
+++ src/com/company/ChooseOption.java
@@ -1,8 +1,9 @@
 package com.company;
 
 import com.company.songs.*;
+import com.company.validations.*;
 
-import java.util.*;
+import java.util.ArrayList;
 
 public class ChooseOption {
 
@@ -10,36 +11,39 @@ public class ChooseOption {
         ArrayList<String> songsDisplay = new ArrayList<String>();
         int choice = -5;
         String keyword;
+        int select;
+
         Display.welcomeMsg();
         Display.mainMenu();
         choice = Validation.readAndValidateChoice();
         Display.cls();
+
         switch (choice) {
             case 1:
                 System.out.println("Songs menu :");
                 Display.listDisplay(Songs.getSongs());
-                int selection = getSelection(Songs.getSongs());
-                SongPlay.playSong(selection);
+                select = Filter.selection(Songs.getSongs());
+                SongPlay.playSong(select);
                 break;
             case 2:
                 System.out.println("Artists available :");
-                filterBy(Songs.getArtists());
+                Filter.filterBy(Songs.getArtists());
                 break;
             case 3:
                 System.out.println("Albums available :");
-                filterBy(Songs.getAlbums());
+                Filter.filterBy(Songs.getAlbums());
                 break;
             case 4:
                 System.out.println("Genres available :");
-                filterBy(Songs.getGenres());
+                Filter.filterBy(Songs.getGenres());
                 break;
             case 5:
                 System.out.println("Search for a song");
                 System.out.print("Write the name of the song and press enter: ");
-                keyword = Validation.validateString();
+                keyword = FilterValidation.validateString();
                 songsDisplay = SongSearch.search(keyword);
-                selection = getSelection(songsDisplay);
-                SongPlay.findSongNumber(songsDisplay, selection);
+                select = Filter.selection(songsDisplay);
+                SongPlay.findSongNumber(songsDisplay, select);
                 break;
             case 6:
                 System.out.println("Good Bye");
@@ -47,36 +51,5 @@ public class ChooseOption {
             default:
                 System.out.println("Incorrect choice: Please enter a valid choice");
         }
-    }
-
-    /**
-     * This method checks whether the entered number is valid or not in the given list.
-     *
-     * @param filterListDisplay list to select the choice
-     * @return choice selected by the user
-     */
-    private static int getSelection(ArrayList<String> filterListDisplay) {
-        int number = Validation.validateChoice(filterListDisplay);
-        if (number == 0) {
-            Display.cls();
-            mainDisplay();
-            System.exit(1);
-        }
-        return number;
-    }
-
-    /**
-     * The method used to filter the list based on the user selection
-     *
-     * @param filterList list which the user want to filter
-     */
-    private static void filterBy(ArrayList<String> filterList) {
-        int selection;
-        ArrayList<String> songsDisplay;
-        Display.listDisplay(SongsList.removeDuplicate(filterList));
-        selection = getSelection(SongsList.removeDuplicate(filterList));
-        songsDisplay = SongsList.displaySongs(SongsList.removeDuplicate(filterList), selection, filterList);
-        selection = getSelection(songsDisplay);
-        SongPlay.findSongNumber(songsDisplay, selection);
     }
 }
--- src/com/company/validations/FilterValidation.java
+++ src/com/company/validations/FilterValidation.java
@@ -1,33 +1,8 @@
-package com.company;
+package com.company.validations;
 
 import java.util.*;
 
-public class Validation {
-
-    public static int readAndValidateChoice() {
-        Scanner sc = new Scanner(System.in);
-        String inputValue;
-        int choice = 50;
-        int check = 0;
-        while (check != 1) {
-            try {
-                inputValue = sc.nextLine();
-                choice = Integer.parseInt(inputValue);
-                while (choice > 6 || choice <= 0) {
-                    System.out.println(" Please enter a number between 1 to 6!");
-                    System.out.print(" Choose an option and press enter: ");
-                    inputValue = sc.nextLine();
-                    choice = Integer.parseInt(inputValue);
-                }
-                check = 1;
-            } catch (NumberFormatException e) {
-                System.out.println(" That's not a number!");
-                System.out.print(" Choose an option and press enter: ");
-            }
-        }
-        return choice;
-    }
-
+public class FilterValidation {
     public static String validateString() {
         Scanner sc = new Scanner(System.in);
         String inputValue = null;

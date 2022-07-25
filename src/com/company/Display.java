5dbfc526a573df09201fdae3e2dae2d5db2ed0c7
sailathatammana <32009979+sailathatammana@users.noreply.github.com>
2022-07-25 13:38:40 +0200

    Feedback update

--- src/com/company/Display.java
+++ src/com/company/Display.java
@@ -11,12 +11,12 @@ public class Display {
 
     public static void mainMenu() {
         System.out.println(" Main menu options:");
-        System.out.println(" 1. Songs");
-        System.out.println(" 2. Artists");
-        System.out.println(" 3. Albums");
-        System.out.println(" 4. Genres");
-        System.out.println(" 5. Search");
-        System.out.println(" 6. Quit\n");
+        System.out.println(" [1] Songs");
+        System.out.println(" [2] Artists");
+        System.out.println(" [3] Albums");
+        System.out.println(" [4] Genres");
+        System.out.println(" [5] Search");
+        System.out.println(" [6] Quit\n");
         System.out.print(" Choose an option and press enter: ");
     }
 
